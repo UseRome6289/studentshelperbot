a4d7ea0c754deafd06882dd0044955698ae057a3
Илья Шульц <anarim2017@mail.ru>
2021-03-18 12:36:49 +0700

    KeyBoards update

--- KeyBoards.py
+++ KeyBoards.py
@@ -1,21 +1,21 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 greet_kb = ReplyKeyboardMarkup(resize_keyboard=True).add("Регистрация")  # Первый
-menu_kb = ReplyKeyboardMarkup(resize_keyboard=True).add("Расписание").add("Профиль").add("Рассылки"). \
-    add("Админ-панель").add("Чат").add("Настройки")  # Меню
+menu_kb = ReplyKeyboardMarkup(resize_keyboard=True).add("Расписание").add("Профиль").add("Рассылки").\
+                                                    add("Админ-панель").add("Чат").add("Настройки")  # Меню
 mailing_lists_kb = ReplyKeyboardMarkup(resize_keyboard=True).add("Меню")  # Рассылки
 profile_kb = ReplyKeyboardMarkup(resize_keyboard=True).add("Меню")  # Профиль
 setting_kb = ReplyKeyboardMarkup(resize_keyboard=True).add("Назад").add("Рассылки"). \
-    add("Изменить информацию")  # Настройки
+                                                        add("Изменить информацию")  # Настройки
 change_information_kb = ReplyKeyboardMarkup(resize_keyboard=True).add("Изменить имя").add("Изменить группу"). \
-    add("Назад")  # Изменение информации о себе
+                                                                     add("Назад")  # Изменение информации о себе
 mailing_lists_kb2 = ReplyKeyboardMarkup(resize_keyboard=True).add(
     "Изменить рассылки")  # Рассылки, в которых состоит пользователь
 admin_panel = ReplyKeyboardMarkup(resize_keyboard=True).add(
     "Отправить рассылку").add("Меню")  # Отправление рассылок из админ-панели
 chat_kb = ReplyKeyboardMarkup(resize_keyboard=True).add(
     "Меню")  # Отправление рассылок из админ-панели
-# keyboards.py
+# Локация и контакт
 markup_request = ReplyKeyboardMarkup(resize_keyboard=True).add(
     KeyboardButton('Отправить свой контакт ☎️', request_contact=True)
 ).add(
